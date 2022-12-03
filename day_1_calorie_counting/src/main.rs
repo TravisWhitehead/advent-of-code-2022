0f8570162d2e0d165f8406851cb136cff175006f
Travis Whitehead <traviswhitehead@hey.com>
2022-12-03 11:40:05 -0800

    refactor(d1): > calories of arbitrary num of elves
    Refactor to be able to get the calories carried by an arbitrary
    number of the most snack-ready elves (instead of from just one elf).

--- day_1_calorie_counting/src/main.rs
+++ day_1_calorie_counting/src/main.rs
@@ -9,7 +9,8 @@ static INPUT_FILE: &str = "../inputs/day1.txt";
 /// A quantity of calories.
 type Calories = u32;
 
-/// List of total calories carried by each elf.
+/// List of total calories carried by each elf sorted by descending snack-readiness.
+#[derive(Debug)]
 struct ElvesCalories(Vec<Calories>);
 
 impl Deref for ElvesCalories {
@@ -21,8 +22,19 @@ impl Deref for ElvesCalories {
 }
 
 impl ElvesCalories {
-    pub fn greatest_calories_carried(&self) -> Option<&Calories> {
-        self.iter().max()
+    /// Construct `ElvesCalories` from a list of calories by sorting by descending snack-readiness.
+    pub fn new(mut elves_calories: Vec<Calories>) -> ElvesCalories {
+        // Sort by descending calories
+        elves_calories.sort();
+        elves_calories.reverse();
+
+        ElvesCalories(elves_calories)
+    }
+
+    /// Returns the calories carried by each of some number of `elves` who are carrying the most calories
+    /// (AKA the most snack-ready).
+    pub fn most_snack_ready(&self, elves: usize) -> &[Calories] {
+        &self[..elves]
     }
 }
 
@@ -37,27 +49,47 @@ impl Display for ElvesCalories {
 
 impl From<Vec<Vec<Calories>>> for ElvesCalories {
     fn from(elves_foods_calories: Vec<Vec<Calories>>) -> Self {
-        ElvesCalories(
-            elves_foods_calories
-                .iter()
-                .map(|elf_foods_calories| elf_foods_calories.iter().sum())
-                .collect(),
-        )
+        // Sum up the calories of the foods each elf is carrying to get the total calories carried by each elf.
+        let elves_calories = elves_foods_calories
+            .iter()
+            .map(|elf_foods_calories| elf_foods_calories.iter().sum())
+            .collect();
+
+        ElvesCalories::new(elves_calories)
     }
 }
 
-/// Returns the greatest calories carried by an elf calculated from the data in `input_file`.
-///
-/// This produces the solution to Day 1 Part 1.
-fn greatest_calories_carried(input_file: &str) -> Calories {
+/// Returns [`ElvesCalories`] parsed from input data in `input_file`.
+fn elves_calories(input_file: &str) -> ElvesCalories {
     let input = fs::read_to_string(input_file).expect("Failed to read input.txt");
 
     // Parse the input into a list of what each elf recorded (a list of calories of foods they are carrying).
     let elves_foods_calories: Vec<Vec<Calories>> = parse_elves_foods_calories(&input);
 
-    let elves_calories = ElvesCalories::from(elves_foods_calories);
+    ElvesCalories::from(elves_foods_calories)
+}
 
-    *elves_calories.greatest_calories_carried().unwrap()
+/// Returns the greatest calories carried by an elf calculated from the data in `input_file`.
+///
+/// This produces the solution to Day 1 Part 1.
+fn greatest_calories_carried(elves_calories: &ElvesCalories) -> Calories {
+    // Return the calories carried by the single most best snack-ready elf
+    elves_calories.most_snack_ready(1_usize)[0]
+}
+
+/// Returns the total sum of the calories carried by the most snack-ready `number_of_elves`.
+///
+/// The most snack-ready elves are the elves carrying the most total calories compared to other elves.
+///
+/// For example, passing `3` for `number_of_elves` returns the total sum of calories carried by the 3 elves
+/// who are carrying the most calories individually compared to the other elves.
+///
+/// This produces the solution to Day 1 Part 2.
+fn total_calories_of_most_snack_ready_elves(
+    elves_calories: &ElvesCalories,
+    elves: usize,
+) -> Calories {
+    elves_calories.most_snack_ready(elves).iter().sum()
 }
 
 fn main() {
