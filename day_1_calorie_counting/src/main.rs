e88553faf9130014b694ec54102455c3c4dc63e3
Travis Whitehead <traviswhitehead@hey.com>
2022-12-03 10:21:02 -0800

    test(d1): Add regression test for day 1 solution

--- day_1_calorie_counting/src/main.rs
+++ day_1_calorie_counting/src/main.rs
@@ -4,6 +4,8 @@ use std::{fmt::Display, fs, ops::Deref};
 
 use parser::parse_elves_calories;
 
+static INPUT_FILE: &str = "../inputs/day1.txt";
+
 /// A quantity of calories.
 type Calories = u32;
 
@@ -44,16 +46,30 @@ impl From<Vec<Vec<Calories>>> for ElvesTotalCalories {
     }
 }
 
-fn main() {
-    let input = fs::read_to_string("./input.txt").expect("Failed to read input.txt");
+/// Returns the greatest calories carried by an elf calculated from the data in `input_file`.
+///
+/// This produces the solution to Day 1 Part 1.
+fn greatest_calories_carried(input_file: &str) -> Calories {
+    let input = fs::read_to_string(input_file).expect("Failed to read input.txt");
 
     // Parse the input into a list of what each elf recorded (a list of calories of foods they are carrying).
     let elves_calories: Vec<Vec<Calories>> = parse_elves_calories(&input);
 
     let elves_total_calories = ElvesTotalCalories::from(elves_calories);
 
-    let elf_with_most_calories = elves_total_calories.greatest_calories_carried().unwrap();
+    *elves_total_calories.greatest_calories_carried().unwrap()
+}
 
-    // println!("{}", elves_total_calories);
-    println!("{}", elf_with_most_calories);
+fn main() {
+    println!("{}", greatest_calories_carried(INPUT_FILE));
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{greatest_calories_carried, INPUT_FILE};
+
+    #[test]
+    fn solve_day_1() {
+        assert_eq!(greatest_calories_carried(INPUT_FILE), 67658);
+    }
 }
