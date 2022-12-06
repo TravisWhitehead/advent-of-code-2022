6957e8e1579a1b5f7a67c6c2856b7f434e7f196a
Travis Whitehead <traviswhitehead@hey.com>
2022-12-05 21:34:15 -0800

    chore(d4): Init day_4_camp_cleanup crate

---
+++ day_4_camp_cleanup/src/main.rs
@@ -0,0 +1,20 @@
+static INPUT_FILE: &str = "inputs/day4.txt";
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    #[ignore]
+    #[test]
+    fn solve_day_4_part_1() {
+        todo!()
+    }
+
+    #[ignore]
+    #[test]
+    fn solve_day_4_part_2() {
+        todo!()
+    }
+}
