cc6e1f91cc97731f610d904bde980c49f0536d91
Travis Whitehead <traviswhitehead@hey.com>
2022-12-07 20:21:29 -0800

    wip

--- day_5_supply_stacks/src/main.rs
+++ day_5_supply_stacks/src/main.rs
@@ -1,8 +1,42 @@
 use std::fs;
 
+use anyhow::Result;
+
 mod parser;
 
 static INPUT_FILE: &str = "inputs/day5.txt";
+
+type Crate = char;
+
+/// Stack of crates.
+struct Stack {
+    /// Label refering to a `Stack` in move instructions.
+    label: u32,
+    crates: Vec<Crate>,
+}
+
+struct Stacks {
+    stacks: HashMap,
+}
+
+impl Stack {}
+
+impl Stacks {
+    /// Construct `Stacks` from rows of crates sorted top-down and associated stack labels.
+    ///
+    /// `crates_grid` is a row of crates where crates may or may not be present.
+    /// Assumes `crates_grid` is ordered from top-down meaning the first row (`crates_grid[0]`)
+    /// represents crates at the top of the stack, and the final row represents crates at the bottom.
+    ///
+    /// `stack_labels`
+    // TODO: Add example of arguments.
+    pub fn from_top_down_crates_rows(
+        crates_rows: Vec<Vec<Some(Crate)>>,
+        stack_labels: Vec<u32>,
+    ) -> Result<Stacks> {
+        let ascending_labels = stack_labels.sort();
+    }
+}
 
 /// Returns the crate that ends up at the top of the stack.
 ///
--- day_5_supply_stacks/src/parser.rs
+++ day_5_supply_stacks/src/parser.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::result_large_err)]
 
 use pear::{
-    combinators::trailing_series,
+    combinators::*,
     input::{Pear, Text},
     macros::{parse_error, parser, switch},
     parsers::*,
@@ -13,29 +13,56 @@ type Input<'a> = Pear<Text<'a>>;
 /// See [`pear::macros::parser`] for details.
 type Result<'a, T> = pear::input::Result<T, Input<'a>>;
 
+struct Move {
+    quantity: u32,
+    from_stack: u32,
+    to_stack: u32,
+}
+
+struct Plan {
+    stack_labels: Vec<u32>,
+    moves: Vec<Move>,
+}
+
 /// Returns true if char is a valid crate label (A through Z).
 ///
 /// This is found in the middle of a stacked crate e.g. [F]
 #[inline]
-fn is_crate_label_char(&byte: &char) -> bool {
-    matches!(byte, 'A'..='Z')
+fn is_crate_label(&c: &char) -> bool {
+    matches!(c, 'A'..='Z')
+}
+
+#[inline(always)]
+fn is_num(&c: &char) -> bool {
+    c.is_ascii_digit()
 }
 
 /// Returns true if the char is a digit char (0, 1, 2, ..., 9)
-#[inline]
-fn is_num_char(&byte: &char) -> bool {
-    matches!(byte, '0'..='9')
+#[inline(always)]
+fn is_space(&c: &char) -> bool {
+    c == ' '
+}
+
+/// Returns true if the char is a space or a newline.
+#[inline(always)]
+fn is_whitespace(&c: &char) -> bool {
+    c == ' ' || c == '\n'
+}
+
+#[parser]
+fn number<'a>(input: &mut Input<'a>) -> Result<'a, u32> {
+    take_some_while(is_num)?
+        .parse()
+        .or_else(|e| parse_error!("{}", e)?);
 }
 
 /// Parses a stacked crate (e.g. `[U]`)
 #[parser]
 fn stacked_crate<'a>(input: &mut Input<'a>) -> Result<'a, char> {
     eat('[')?;
-    let label = eat_if(is_crate_label_char)
+    let label = eat_if(is_crate_label)
         .or_else(|_| parse_error!("Expected crate label to be char A through Z"))?;
     eat(']')?;
-    // surrounded(is_crate_label_char, '')
-    // delimited_some('[', is_crate_label_char, ']').map(|label| label[0b0])?
     label
 }
 
@@ -54,9 +81,18 @@ fn some_stacked_crate<'a>(input: &mut Input<'a>) -> Result<'a, Option<char>> {
 /// Example:
 /// `[Z] [M]     [R]`
 #[parser]
-fn stacked_crates_row<'a>(input: &mut Input<'a>) -> Result<'a, Vec<Option<char>>> {
+fn stacked_crates_line<'a>(input: &mut Input<'a>) -> Result<'a, Vec<Option<char>>> {
     let row: Vec<Option<char>> = trailing_series(some_stacked_crate, ' ')?;
     row
+}
+
+/// Parses a stack label (a number indicating the column of the stack) that might be surrounded by spaces.
+#[parser]
+fn stack_label<'a>(input: &mut Input<'a>) -> Result<'a, u32> {
+    skip_while(is_space)?;
+    let label = number()?;
+    skip_while(is_space)?;
+    label
 }
 
 /// Parses the label numbers for each stack of crates.
@@ -67,9 +103,9 @@ fn stacked_crates_row<'a>(input: &mut Input<'a>) -> Result<'a, Vec<Option<char>>
 ///  1   2   3
 /// ```
 #[parser]
-fn stack_labels<'a>(input: &mut Input<'a>) -> Result<'a, Vec<char>> {
-    // let labels: Vec<char> = trailing_series(is_num_char, skip_while(' '))?;
-    todo!()
+fn stack_labels_line<'a>(input: &mut Input<'a>) -> Result<'a, Vec<u32>> {
+    let labels: Vec<u32> = try_collect(stack_label)?;
+    labels
 }
 
 /// Parses the beginning segment of the input containing a drawing of the arrangement of stacked crates.
@@ -82,7 +118,32 @@ fn stack_labels<'a>(input: &mut Input<'a>) -> Result<'a, Vec<char>> {
 ///  1   2   3
 /// ```
 #[parser]
-fn stacked_crates_drawing<'a>(input: &mut Input<'a>) -> Result<'a, Vec<Vec<Option<char>>>> {
-    let stacked_crates_rows: Vec<Vec<Option<char>>> = trailing_series(stacked_crates_row, ' ')?;
-    stacked_crates_rows
+fn stacked_crates_drawing<'a>(
+    input: &mut Input<'a>,
+) -> Result<'a, (Vec<Vec<Option<char>>>, Vec<u32>)> {
+    let stacked_crates_rows = trailing_series(stacked_crates_line, ' ')?;
+    let stack_labels = stack_labels_line()?;
+    skip_while(is_whitespace)?;
+    (stacked_crates_rows, stack_labels)
 }
+
+#[parser]
+fn move_line<'a>(input: &mut Input<'a>) -> Result<'a, Move> {
+    eat_slice("move ")?;
+    let quantity = number()?;
+    eat_slice(" from ")?;
+    let from_stack = number()?;
+    eat_slice(" to ")?;
+    let to_stack = number()?;
+    let move_crates = Move {
+        quantity,
+        from_stack,
+        to_stack,
+    };
+    eat('\n')?;
+    move_crates
+}
+
+/// Parse the full plan input.
+#[parser]
+fn plan<'a>(input: &mut Input<'a>) -> Result<'a, Move> {}
